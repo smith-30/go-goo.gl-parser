f86c7f77405a12f30e360eddfdf9881bf992bef0
misu <parkour12019@gmail.com>
2017-12-08 22:59:20 +0900

    add

---
+++ example.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/smith-30/go-goo.gl-parser/goo.gl/parser"
+)
+
+func main() {
+	p := parser.NewParser("your api token")
+	d, err := p.DecodeURL("https://goo.gl/kMxDaw")
+	if err != nil {
+		fmt.Printf("%v\n", err)
+	}
+
+	fmt.Println(d) // https://github.com/smith-30/go-goo.gl-parser
+}
